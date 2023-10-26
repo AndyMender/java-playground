5c007cc2564d898c342637b38625aafb21c9b261
Andy Mender <andymenderunix@gmail.com>
2023-10-26 17:11:12 +0200

    Extend data conversions

--- codecademy/intro_course/DataConvertion.java
+++ codecademy/intro_course/DataConvertion.java
@@ -10,5 +10,12 @@ public class DataConvertion {
         // NOTE: No difference in output
         String string = String.valueOf(num);
         System.out.println("Converted string value: " + string);
+        
+        // Convert String back to an int
+        // NOTE: Calling .parseInt() on an invalid integer will throw
+        int numBack = Integer.parseInt(string);
+        System.out.println("Pure int value again: " + numBack);
+        numBack++;
+        System.out.println("Pure int value again: " + numBack);
     }
 }
