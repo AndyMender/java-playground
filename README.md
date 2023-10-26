d4988f76b6236129f590ed5e8f2b8c56cfa2d018
Andy Mender <andymenderunix@gmail.com>
2023-10-26 15:46:55 +0200

    Update README, add 1 simple class

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # java-playground
 playground repository for Java code
+
+## Building
+```bash
+javac dir/subdir/class.java
+```
+
+## Running
+```bash
+# Using the package notation
+java dir.subdir.class
+```
---
+++ codecademy/intro_course/Compiling.java
@@ -0,0 +1,9 @@
+package codecademy.intro_course;
+
+public class Compiling {
+    // Assumes class is the main executor in an app/binary
+    public static void main(String[] args) {
+        System.out.println("Java is a class/component based language");
+        System.out.println("This is the main method of the Compiling class.");
+    }
+}
