b785fb6f952cb04b7569ba5707309d84fce86e6e
Andy Mender <andymenderunix@gmail.com>
2023-10-28 19:00:02 +0200

    Use printf in favor of simplicity

--- codecademy/intro_course/Accounting.java
+++ codecademy/intro_course/Accounting.java
@@ -8,35 +8,34 @@ public class Accounting {
         this.accountBalance = startingBalance;
 
         System.out.println("Welcome to Java Bank!");
-        System.out.println(String.format(
-            "You have opened an account with a starting balance of %.2f.",
+        System.out.printf("You have opened an account with a starting balance of %.2f.\n",
             this.accountBalance
-        ));
+        );
     }
 
     public void checkBalance() {
-        System.out.println(String.format("Hello! Your account balance is %.2f.", this.accountBalance));
+        System.out.printf("Hello! Your account balance is %.2f.\n", this.accountBalance);
 
         if (this.accountBalance < 0) {
-            System.out.println(String.format(
-                    "Oh dear... your account balance is now in red! You have to pay back %.2f.",
+            System.out.printf(
+                    "Oh dear... your account balance is now in red! You have to pay back %.2f.\n",
                     Math.abs(this.accountBalance)
-            ));
+            );
         }
     }
 
     public void deposit(double amountToDeposit) {
         this.accountBalance += amountToDeposit;
-        System.out.println(String.format("You just deposited %.2f.", amountToDeposit));
+        System.out.printf("You just deposited %.2f.\n", amountToDeposit);
     }
 
     public double withdraw(int amountToWithdraw) {
         // 1. Check for validity and allow overdrawing
         if ((this.accountBalance - amountToWithdraw) < 0) {
-            System.err.println(String.format(
-                "You are trying to withdraw %.2f and you only have %.2f. Your account is now in debit!",
+            System.err.printf(
+                "You are trying to withdraw %.2f and you only have %.2f. Your account is now in debit!\n",
                 amountToWithdraw, this.accountBalance
-            ));
+            );
         }
 
         // 2. Adjust balance
@@ -44,7 +43,7 @@ public class Accounting {
         this.accountBalance -= amountToWithdraw;
         
         // 3. Withdraw money
-        System.out.println(String.format("You just withdrew %f.", amountToWithdraw));
+        System.out.printf("You just withdrew %.2f.\n", amountToWithdraw);
         return amountToWithdraw;
     }
 
