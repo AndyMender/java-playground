85a267ef671925c75b6271bfa1057c69221e4305
Andy Mender <andymenderunix@gmail.com>
2024-03-09 22:40:37 +0100

    Add extra error handling to UserInputter

--- codecademy/intermediate_course/UserInputter.java
+++ codecademy/intermediate_course/UserInputter.java
@@ -4,17 +4,22 @@ package codecademy.intermediate_course;
 import java.util.Scanner;
 
 public class UserInputter {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NullPointerException {
         // Scanner supports a look-ahead check if a value of a given type is available
         // and reading all value types one by one, from a string, file and stdin.
         // A delimiter can be set to parse simple data files (for instance, CSV)
-        Scanner input = new Scanner(System.in);
-        String userName = "player";
+        try (var input = new Scanner(System.in)) {
+            String userName = null;
 
-        System.out.print("What is your name traveler? ");
-        if (input.hasNext()) {
-            userName = input.next();
+            System.out.print("What is your name traveler? ");
+            if (input.hasNext()) {
+                userName = input.next();
+            }
+            if (userName != null || userName == "") {
+                System.out.printf("Welcome to the Wilderness %s...\n", userName);
+            } else {
+                throw new NullPointerException("Couldn't read name correctly: " + userName);
+            }
         }
-        System.out.printf("Welcome to the Wilderness %s...\n", userName);
     }
 }
