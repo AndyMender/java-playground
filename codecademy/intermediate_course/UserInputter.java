29af276a2a010a6a198d73bf19a7216b5f046c01
Andy Mender <andymenderunix@gmail.com>
2024-03-10 11:37:01 +0100

    Simplify logic in UserInputter - whitespace is handled already

--- codecademy/intermediate_course/UserInputter.java
+++ codecademy/intermediate_course/UserInputter.java
@@ -1,10 +1,11 @@
 package codecademy.intermediate_course;
 
+import java.io.IOException;
 // NOTE: java.lang is imported by default, java.util and java.io are not!
 import java.util.Scanner;
 
 public class UserInputter {
-    public static void main(String[] args) throws NullPointerException {
+    public static void main(String[] args) throws IOException {
         // Scanner supports a look-ahead check if a value of a given type is available
         // and reading all value types one by one, from a string, file and stdin.
         // A delimiter can be set to parse simple data files (for instance, CSV)
@@ -15,10 +16,11 @@ public class UserInputter {
             if (input.hasNext()) {
                 userName = input.next();
             }
-            if (userName != null || userName == "") {
+            // NOTE: Added a check for safety, but 'hasNext()' demands valid input and strips all whitespace
+            if (userName != null) {
                 System.out.printf("Welcome to the Wilderness %s...\n", userName);
             } else {
-                throw new NullPointerException("Couldn't read name correctly: " + userName);
+                throw new IOException("Couldn't read name correctly: " + userName);
             }
         }
     }
