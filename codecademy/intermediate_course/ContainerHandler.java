69c43f445cc18110d813c70ca2ea7c051e9d982a
Andy Mender <andymenderunix@gmail.com>
2024-03-10 15:51:02 +0100

    Add notes on auto-boxing to ContainerHandler

--- codecademy/intermediate_course/ContainerHandler.java
+++ codecademy/intermediate_course/ContainerHandler.java
@@ -3,13 +3,20 @@ package codecademy.intermediate_course;
 public class ContainerHandler {
     public static void main(String[] args) {
         String word = "Hello";
+        int number = 56;
         Book book = new Book("Fire and the Wolf");
         
         // Demonstrate the use of generics
         Container<String> wordBox = new Container<>(word);
+        // NOTE: 'number' is auto-boxed to the 'Integer' type,
+        //       using 'Integer.valueOf(number)'
+        // 
+        // Unboxing can be done using 'Integer.intValue()'
+        Container<Integer> intBox = new Container<>(number);
         Container<Book> bookBox = new Container<>(book);
 
         System.out.println("Box of words: " + wordBox.getData());
+        System.out.println("Box of numbers: " + intBox.getData());
         System.out.println("Box of books: " + bookBox.getData());
     }
 }
