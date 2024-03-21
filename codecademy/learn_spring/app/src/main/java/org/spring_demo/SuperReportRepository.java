9f7bc236e3a36ac95826c453b5c2ffdc5ac50639
Andy Mender <andymenderunix@gmail.com>
2024-03-21 23:40:03 +0100

    Add filtered getters to SuperReportRepository

--- codecademy/learn_spring/app/src/main/java/org/spring_demo/SuperReportRepository.java
+++ codecademy/learn_spring/app/src/main/java/org/spring_demo/SuperReportRepository.java
@@ -2,6 +2,7 @@ package org.spring_demo;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class SuperReportRepository {
     private List<SuperReport> repository = new ArrayList<>(); 
@@ -10,5 +11,30 @@ public class SuperReportRepository {
         repository.add(heroReport);
     }
 
+    // TODO: Should return a copy instead of a reference?
+    public List<SuperReport> findAll() {
+        return repository;
+    }
+    
+    public List<SuperReport> findByPostalCode(String postalCode) {
+        return repository
+            .stream()
+            .filter(x -> x.getPostalCode().equals(postalCode))
+            .collect(Collectors.toList());
+    }
 
+    public List<SuperReport> findByStreetAddress(String streetAddress) {
+        return repository
+            .stream()
+            .filter(x -> x.getStreetAddress().equals(streetAddress))
+            .collect(Collectors.toList());
+    }
+
+    public List<SuperReport> findByHeroName(String heroName) {
+        // It's okay for 'heroName' to be empty - multiple returned records are supported
+        return repository
+            .stream()
+            .filter(x -> x.getHeroName().equals(heroName))
+            .collect(Collectors.toList());
+    }
 }
