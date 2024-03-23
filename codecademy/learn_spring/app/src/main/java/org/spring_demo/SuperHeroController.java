08c3c17710f2c794e1027480d1b91a70c36f1b45
Andy Mender <andymenderunix@gmail.com>
2024-03-23 23:46:24 +0100

    Add main SuperHeroController code

---
+++ codecademy/learn_spring/app/src/main/java/org/spring_demo/SuperHeroController.java
@@ -0,0 +1,68 @@
+package org.spring_demo;
+
+import java.lang.Iterable;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;   // enum containing GET, POST, DELETE, etc.
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+// NOTE: The default HTTP 'method' is 'RequestMethod.GET'
+@RestController
+@RequestMapping("/superHeroes")
+public class SuperHeroController {
+  // TODO: The classes need to be implemented separately
+  private final SuperHeroRepository superHeroRepository;
+  private final SuperReportRepository superReportRepository;
+
+  // TODO: The repository objects can be fed from the main 'App' code
+  public SuperHeroController(SuperHeroRepository superHeroRepository, SuperReportRepository superReportRepository) {
+    this.superHeroRepository = superHeroRepository;
+    this.superReportRepository = superReportRepository;
+  }
+
+  // Main endpoint
+  // NOTE: In addition to 'GetMapping' and 'PostMapping', there are wrappers for other HTTP methods
+  @GetMapping()
+  public Iterable<SuperHero> getSuperHeros() {
+    // TODO: 'SuperHero' class needs an implementation
+    Iterable<SuperHero> superHeroes = superHeroRepository.findAll();
+    return superHeroes;
+  }
+
+  // 'RequestParam' is used to consume URL query parameters
+  @PostMapping("/addNew")
+  public String createNewSuperHero(@RequestBody SuperHero superHero) {
+    superHeroRepository.save(superHero);
+    return "New Super Hero successfully added!";
+  }
+
+  @PostMapping("/help")
+  public String postHelp(@RequestParam String postalCode, @RequestParam String streetAddress) {
+    // TODO: 'SuperReport' class needs an implementation
+    SuperReport newSuperReport = new SuperReport(postalCode, streetAddress, "");
+    superReportRepository.save(newSuperReport);
+    return String.format(
+      "Thanks! Super Heroes have been dispatched to %s, %s!",
+      streetAddress,
+      postalCode
+    );
+  }
+
+  @GetMapping("/heroReport")
+  public Iterable<SuperReport> getHeroReport() {
+    Iterable<SuperReport> superReport = superReportRepository.findAll();
+    return superReport;
+  }
+
+  // parametrized mapping
+  @PostMapping("/{postalCode}")
+  public Iterable<SuperReport> getHeroReportByPostal(@PathVariable String postalCode) {
+    Iterable<SuperReport> superReport = superReportRepository.findByPostalCode(postalCode);
+    return superReport;
+  }
+}
