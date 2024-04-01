1180827ea6a5ee8d21aec1864bb07db4a7adf0d5
Andy Mender <andymenderunix@gmail.com>
2024-04-01 12:09:12 +0200

    Add initial hooks for Spring Boot to main App

--- codecademy/build_spring_app/app/src/main/java/build_spring_app/App.java
+++ codecademy/build_spring_app/app/src/main/java/build_spring_app/App.java
@@ -3,14 +3,39 @@
  */
 package build_spring_app;
 
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
 public class App {
     public String getClassName() {
-        // TODO: Return only the last part/suffix of the class name?
         return this.getClass().getName();
     }
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
+        System.out.println(String.format("Starting Spring Boot app %s...", new App().getClassName()));
 
-        System.out.println(new App().getClassName());
-    }
+		SpringApplication.run(App.class, args);
+	}
+
+    // Prints out all of the beans loaded as part of Spring Boot app startup
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+
+			System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+
+		};
+	}
 }
